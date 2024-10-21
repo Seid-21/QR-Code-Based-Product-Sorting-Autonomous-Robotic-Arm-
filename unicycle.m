f0126833dee8d6524426cf8c281e57773b0ac432
Seid-21 <fikrueshetu47@gmail.com>
2024-10-21 22:46:12 +0200

    Update unicycle.m

--- unicycle.m
+++ unicycle.m
@@ -1,10 +1,1 @@
-function out = unicycle(in)
-% Input: velocities of the robot platform [v_x; omega]
-% Output: velocities in the global frame [x'; y'; theta']
-    
-    th =  ;
-    u = ;
-    dq = ;
-    out = ;
-    
-return;
+
